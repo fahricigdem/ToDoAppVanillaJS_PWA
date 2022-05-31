b8a23e1886f7b0360b352dab1036e56aee89acdf
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2022-05-31 23:25:00 +0200

    sw.js spring fetch

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ var cacheList = [
 
 self.addEventListener('install', e => e.waitUntil(caches.open("static").then(cache => cache.addAll(cacheList))))
 
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(response => response || fetch(e.request.url))))
+//self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(response => response || fetch(e.request.url))))
 
 
 
