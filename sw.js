0579fd23e59f8e131fe9243b1bf99bd1c961e01c
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2022-06-06 22:58:01 +0200

    test

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var cacheList = [
     "icon.png",
 ]
 
-//self.addEventListener('install', e => e.waitUntil(caches.open("static").then(cache => cache.addAll(cacheList))))
+self.addEventListener('install', e => e.waitUntil(caches.open("static").then(cache => cache.addAll(cacheList))))
 
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(response => response || fetch(e.request.url))))
 
